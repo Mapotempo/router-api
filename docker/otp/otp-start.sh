d02d04cbfcb40119507ff57acf45b8f1febd5c5b
Alexis Lahouze <xals@lahouze.org>
2017-06-27 18:02:53 +0200

    Add OTP docker files.

---
+++ docker/otp/otp-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DATA=/srv/otp/data
+
+ROUTING=$(for c in ${DATA}/graphs/*; do echo " --router $(realpath --relative-to=${DATA}/graphs $c)"; done)
+
+exec java -Xmx4G -jar ${OTP} --basePath ${DATA} --analyst --server --port 7000 --insecure ${ROUTING}
