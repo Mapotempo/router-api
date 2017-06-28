37bc581d65e6c165c27c6f72d152d5d2598c0881
Alexis Lahouze <xals@lahouze.org>
2017-06-28 23:03:39 +0200

    Add OSRM Docker files.

---
+++ docker/osrm/server/osrm-load.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+die() {
+    echo $*
+    exit 1
+}
+
+DATADIR=/srv/osrm/data
+DATA_LINK=${DATADIR}/${REGION}-${PROFILE}-latest.osrm
+OSRM_FILE=$(/bin/readlink -e ${DATA_LINK})
+
+[ $? -eq 1 ] && die "${DATA_LINK} target not found."
+
+exec /usr/bin/osrm-datastore ${OSRM_FILE}
---
+++ docker/osrm/server/osrm-start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+die() {
+    echo $*
+    exit 1
+}
+
+# Initialize environment for daemons.
+for daemon in routed isochrone; do
+    mkdir -p /etc/service/${daemon}/env
+    echo ${PROFILE} > /etc/service/${daemon}/env/PROFILE
+    echo ${REGION} > /etc/service/${daemon}/env/REGION
+done
+
+# Specifig environment for Isochrone.
+echo ${NODE_CONFIG} > /etc/service/isochrone/env/NODE_CONFIG
+
+# Run datastore once with delay
+osrm-load.sh ${PROFILE} ${REGION}
+
+exec /usr/bin/runsvdir -P /etc/service
