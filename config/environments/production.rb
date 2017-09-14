2151e7d5c83bf8cadc69bb849813bff983b38e7e
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2017-09-14 15:44:36 +0200

    Fix default prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ module RouterWrapper
 
   DEMO = Wrappers::Demo.new(CACHE)
   OSRM5 = Wrappers::Osrm5.new(CACHE, url_time: 'http://router.project-osrm.org', url_distance: 'http://router.project-osrm.org', url_isochrone: 'http://localhost:1723', url_isodistance: 'http://localhost:1723', licence: 'ODbL', attribution: '© OpenStreetMap contributors')
-  OSRM5_CAR_ICELAND = Wrappers::Osrm5.new(CACHE, url_time: 'http://osrm-car-iceland:500', url_distance: nil, url_isochrone: 'http://osrm-car-iceland:6000', url_isodistance: nil, licence: 'ODbL', attribution: '© OpenStreetMap contributors')
+  OSRM5_CAR_ICELAND = Wrappers::Osrm5.new(CACHE, url_time: 'http://osrm-car-iceland:5000', url_distance: nil, url_isochrone: 'http://osrm-car-iceland:6000', url_isodistance: nil, licence: 'ODbL', attribution: '© OpenStreetMap contributors')
   OTP_BORDEAUX = Wrappers::Otp.new(CACHE, url: 'http://localhost:8080', router_id: 'bordeaux', licence: 'ODbL', attribution: 'Bordeaux Métropole', area: 'Bordeaux', crs: 'EPSG:2154')
   HERE_APP_ID = nil
   HERE_APP_CODE = nil
