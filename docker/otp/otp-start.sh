2f5ea50f0293a2ed4f2185fb4631b20d108a5f05
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2017-10-02 14:04:55 +0200

    Docker: 6GB for OTP

--- docker/otp/otp-start.sh
+++ docker/otp/otp-start.sh
@@ -4,4 +4,4 @@ DATA=/srv/otp/data
 
 ROUTING=$(for c in ${DATA}/graphs/*; do echo " --router $(realpath --relative-to=${DATA}/graphs $c)"; done)
 
-exec java -Xmx4G -jar ${OTP} --basePath ${DATA} --analyst --server --port 7000 --insecure ${ROUTING}
+exec java -Xmx6G -jar ${OTP} --basePath ${DATA} --analyst --server --port 7000 --insecure ${ROUTING}
