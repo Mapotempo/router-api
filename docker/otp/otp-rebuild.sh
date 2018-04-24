d905ea1578a4d31391cb896374ab4d6d909ef680
Alexis Lahouze <alexis@lahouze.org>
2018-04-24 10:20:50 +0200

    Fix absolute path.

--- docker/otp/otp-rebuild.sh
+++ docker/otp/otp-rebuild.sh
@@ -7,7 +7,7 @@ DIR=$(dirname $0)
 set -e
 
 echo "Generate graph."
-docker-compose --project-directory ${DIR}/../ --project-name router run --rm --entrypoint make otp-${ROUTER} -C ${DIR}/data/graphs/${ROUTER}/
+docker-compose --project-directory ${DIR}/../ --project-name router run --rm --entrypoint make otp-${ROUTER} -C /srv/otp/data/graphs/${ROUTER}/
 
 echo "Wait 10 seconds before reloading graph."
 sleep 10
