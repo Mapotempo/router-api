3386106d53d27c077c9b03b25f937cbf80e4bd4e
Alexis Lahouze <alexis@lahouze.org>
2018-04-24 13:55:20 +0200

    Remove reload part from otp-rebuild.sh.

--- docker/otp/otp-rebuild.sh
+++ docker/otp/otp-rebuild.sh
@@ -8,10 +8,3 @@ set -e
 
 echo "Generate graph."
 docker-compose --project-directory ${DIR}/../ --project-name router run --rm --entrypoint make otp-${ROUTER} -C /srv/otp/data/graphs/${ROUTER}/
-
-echo "Wait 10 seconds before reloading graph."
-sleep 10
-
-echo "Reload graph."
-PORT=$(docker inspect router_otp-${ROUTER}_1 | jq -r '.[0].NetworkSettings.Ports["7000/tcp"][0].HostPort')
-curl -X PUT http://localhost:${PORT}/otp/routers/${ROUTER}
