b0d214224bddb54410edb8f585d75e88b57a05fe
Alain ANDRÉ <wordsbybird@alain-andre.fr>
2019-08-28 17:59:24 +0200

    Docker : configure logger

--- docker/otp/otp-start.sh
+++ docker/otp/otp-start.sh
@@ -4,4 +4,5 @@ DATA=/srv/otp/data
 
 ROUTING=$(for c in ${DATA}/graphs/*; do echo " --router $(realpath --relative-to=${DATA}/graphs $c)"; done)
 
+exec /usr/bin/logger -t ${OTP} --server ${LOGGER_SERVER} --port ${LOGGER_PORT}
 exec java -Xmx6G -jar ${OTP} --basePath ${DATA} --analyst --server --port 7000 --insecure ${ROUTING}
