c31dc4395d75bf5fb64030c8d3ad2f42f6b1eb56
Alain ANDRE <wordsbybird@alain-andre.fr>
2021-12-17 14:58:26 +0100

    Api: fixes matrix count

--- test/api/v01/api_base_test.rb
+++ test/api/v01/api_base_test.rb
@@ -22,20 +22,20 @@ require './api/root'
 class Api::V01::APIBaseTest < Minitest::Test
   def test_count_locations
     lat, lng = 1, 1
-    assert 2, Api::V01::APIBase.count_locations([[[lat, lng], [lat, lng]]])
-    assert 2, Api::V01::APIBase.count_locations([[lat, lng], [lat, lng]])
-    assert 2, Api::V01::APIBase.count_locations([lat, lng, lat, lng])
-    assert 2, Api::V01::APIBase.count_locations('lat,lng,lat,lng')
-    assert 0, Api::V01::APIBase.count_locations(nil)
+    assert_equal 2, Api::V01::APIBase.count_locations([[[lat, lng], [lat, lng]]])
+    assert_equal 2, Api::V01::APIBase.count_locations([[lat, lng], [lat, lng]])
+    assert_equal 2, Api::V01::APIBase.count_locations([lat, lng, lat, lng])
+    assert_equal 2, Api::V01::APIBase.count_locations('lat,lng,lat,lng')
+    assert_equal 0, Api::V01::APIBase.count_locations(nil)
   end
 
   def test_count_matrix_locations
-    assert 2, Api::V01::APIBase.count_matrix_locations(src: 'lat,lng,lat,lng', dest: 'lat,lng,lat,lng')
-    assert 2, Api::V01::APIBase.count_matrix_locations(src: 'lat,lng,lat,lng')
+    assert_equal 2, Api::V01::APIBase.count_matrix_locations(src: 'lat,lng,lat,lng', dest: 'lat,lng')
+    assert_equal 12, Api::V01::APIBase.count_matrix_locations(src: 'lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng')
   end
 
   def test_count_route_locations
-    assert 2, Api::V01::APIBase.count_matrix_locations(loc: 'lat,lng,lat,lng')
-    assert 2, Api::V01::APIBase.count_matrix_locations(locs: 'lat,lng,lat,lng')
+    assert_equal 2, Api::V01::APIBase.count_route_locations(loc: 'lat,lng,lat,lng')
+    assert_equal 12, Api::V01::APIBase.count_route_locations(locs: 'lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng,lat,lng')
   end
 end
